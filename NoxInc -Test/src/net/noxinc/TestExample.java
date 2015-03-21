5fe28e4502f866117e1cbad81c529b727f58d971
NoxInc <yolo632@byom.de>
2015-03-21 16:18:36 +0100

    Updated for tests

--- NoxInc -Test/src/net/noxinc/TestExample.java
+++ NoxInc -Test/src/net/noxinc/TestExample.java
@@ -1,10 +1,14 @@
 package net.noxinc;
 
+import net.noxinc.world.Map;
+
 public class TestExample 
 {
 	public static void main(String[] args)
 	{
+		Map a1 = new Map();
 		
+		a1.drawMap();
 	}
 
 }
