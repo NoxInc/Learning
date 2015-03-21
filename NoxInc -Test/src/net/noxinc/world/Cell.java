0ba6932a3bd6e3a9b39f0fb4d45a3d5599d29b23
NoxInc <yolo632@byom.de>
2015-03-21 16:18:18 +0100

    added base functionality

--- NoxInc -Test/src/net/noxinc/world/Cell.java
+++ NoxInc -Test/src/net/noxinc/world/Cell.java
@@ -1,5 +1,21 @@
 package net.noxinc.world;
 
-public class Cell {
-
+public class Cell 
+{
+	private char symbol;
+	
+	public Cell(char symbol)
+	{
+		this.symbol = symbol;
+	}
+	
+	public Cell()
+	{
+		
+	}
+	
+	public char getSymbol()
+	{
+		return symbol;
+	}
 }
--- NoxInc -Test/src/net/noxinc/world/Map.java
+++ NoxInc -Test/src/net/noxinc/world/Map.java
@@ -1,5 +1,54 @@
 package net.noxinc.world;
 
-public class Map {
+public class Map 
+{
+	private Cell[][] board;
+	
+	public Map(int x, int y)
+	{
+		board = new Cell[y][x];
+		createMap();
+	}
+	
+	public Map()
+	{
+		board = new Cell[5][5];
+		createMap();
+	}
+	
+	public void drawMap()
+	{
+		String tmp = "";
+		for(int x = 0; x < board.length; x++)
+		{
+			for(int y = 0; y < board[x].length; y++)
+			{
+				if(board[x][y] == null)
+				{
+					tmp += ".";
+				}else{
+					tmp += board[x][y].getSymbol();
+				}
+			}
+			tmp += "\n";
+		}
+		System.out.println(tmp);
+	}
+	
+	public void createMap()
+	{
+		for(int x = 1; x < board.length - 1; x++)
+		{
+			for(int y = 1; y < board[x].length - 1; y++)
+			{
+				createCellAtLocation(new Cell(' '),x , y);
+			}
+		}
+	}
+	
+	public void createCellAtLocation(Cell cell, int x, int y)
+	{
+		board[x][y] = cell;
+	}
 
 }
