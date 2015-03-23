e84a70407bf9c96fecc72ada51e48e31e47e68cd
NoxInc <yolo632@byom.de>
2015-03-23 16:41:16 +0100

    Added core functionality and debug Cells

--- NoxInc -Test/src/net/noxinc/TestExample.java
+++ NoxInc -Test/src/net/noxinc/TestExample.java
@@ -1,14 +1,53 @@
 package net.noxinc;
 
+import java.util.Scanner;
+
+import net.noxinc.units.Player;
+import net.noxinc.world.Cell;
 import net.noxinc.world.Map;
 
 public class TestExample 
 {
 	public static void main(String[] args)
 	{
-		Map a1 = new Map();
+		Scanner myScanner = new Scanner(System.in);
+		Map a1 = new Map(5, 8);
+		Player p1 = new Player(a1, 2, 2);
 		
+		a1.createCellAtLocation(new Cell('x', "TestSolid") .setSolidness(true), 3, 3);
+		a1.createCellAtLocation(new Cell('H', "Healing Pot")
+		{
+			@Override
+			public void onArrival()
+			{
+				p1.setHealth(p1.getHealth() + 5);
+				System.out.println(p1.getHealth());
+			}
+		}, 3, 0);
 		a1.drawMap();
+		
+		while(p1.getHealth() > 0)
+		{
+			int tmp = myScanner.nextInt();
+			switch(tmp)
+			{
+			case 1:
+				p1.moveTo(-1, 0);
+				break;
+			case 2:
+				p1.moveTo(1, 0);
+				break;
+			case 3:
+				p1.moveTo(0, 1);
+				break;
+			case 4:
+				p1.moveTo(0, -1);
+				break;
+			default:
+				System.out.println("No valid option");
+			}
+			a1.drawMap();
+		}
 	}
 
 }
