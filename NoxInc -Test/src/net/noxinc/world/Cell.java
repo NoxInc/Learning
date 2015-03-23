11ab78ed0ba6f284dc34eb4c0162e756979c0e99
NoxInc <yolo632@byom.de>
2015-03-23 16:40:09 +0100

    Added onArrival and others
    getName
    isSolid
    setSolidness

--- NoxInc -Test/src/net/noxinc/world/Cell.java
+++ NoxInc -Test/src/net/noxinc/world/Cell.java
@@ -3,13 +3,21 @@ package net.noxinc.world;
 public class Cell 
 {
 	private char symbol;
+	private boolean solid;
+	private String name;
 	
-	public Cell(char symbol)
+	public Cell(char symbol, String name)
 	{
 		this.symbol = symbol;
+		this.name = name;
 	}
 	
 	public Cell()
+	{
+		
+	}
+	
+	public void onArrival()
 	{
 		
 	}
@@ -18,4 +26,21 @@ public class Cell
 	{
 		return symbol;
 	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public boolean isSolid()
+	{
+		return solid;
+	}
+	
+	public Cell setSolidness(boolean value)
+	{
+		solid = value;
+		return this;
+	}
+	
 }
