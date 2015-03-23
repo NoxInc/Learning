baee3b67d16854988d105e2b684ebffa097a53d8
NoxInc <yolo632@byom.de>
2015-03-23 21:20:16 +0100

    Fixed movement

--- NoxInc -Test/src/net/noxinc/units/Player.java
+++ NoxInc -Test/src/net/noxinc/units/Player.java
@@ -27,7 +27,7 @@ public class Player extends Cell
 	
 	public void moveTo(int offsetX, int offsetY)
 	{
-		if((!(x + offsetX > map.getXLength() || x + offsetX < 0) && !(y + offsetY > map.getYLength() || y + offsetY < 0)))
+		if((!(x + offsetX > map.getYLength() - 1 || x + offsetX < 0) && !(y + offsetY > map.getXLength() - 1 || y + offsetY < 0)))
 		{
 			if(map.getCellAtPosition(x + offsetX, y + offsetY) == null || map.getCellAtPosition(x + offsetX, y + offsetY).isSolid())
 			{
@@ -35,12 +35,14 @@ public class Player extends Cell
 			}
 			else
 			{
-				map.getCellAtPosition(x + offsetX, y + offsetY).onArrival();
+				map.getCellAtPosition(x + offsetX, y + offsetY).onArrival(this);
 				map.createCellAtLocation(this, x + offsetX, y + offsetY);
 				map.createCellAtLocation(new Cell(' ', ""), x, y);
 				x += offsetX;
 				y += offsetY;
 			}
+		}else{
+			System.out.println("Reached end of map");
 		}
 	}
 	
