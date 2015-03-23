d929f14bb08fa5a6448cbb092ab5ca919c7b4530
NoxInc <yolo632@byom.de>
2015-03-23 21:20:00 +0100

    Added healing and dmg

--- NoxInc -Test/src/net/noxinc/world/Cell.java
+++ NoxInc -Test/src/net/noxinc/world/Cell.java
@@ -1,9 +1,13 @@
 package net.noxinc.world;
 
+import net.noxinc.units.Player;
+
 public class Cell 
 {
 	private char symbol;
 	private boolean solid;
+	private int dmg;
+	private int heal;
 	private String name;
 	
 	public Cell(char symbol, String name)
@@ -17,9 +21,15 @@ public class Cell
 		
 	}
 	
-	public void onArrival()
+	public void onArrival(Player player)
 	{
-		
+		if(dmg > 0)
+		{
+			player.setHealth(player.getHealth() - dmg);
+		}else if(heal > 0)
+		{
+			player.setHealth(player.getHealth() + heal);
+		}
 	}
 	
 	public char getSymbol()
@@ -40,6 +50,18 @@ public class Cell
 	public Cell setSolidness(boolean value)
 	{
 		solid = value;
+		return this;
+	}
+	
+	public Cell setDamage(int dmg)
+	{
+		this.dmg = dmg;
+		return this;
+	}
+	
+	public Cell setHealing(int heal)
+	{
+		this.heal = heal;
 		return this;
 	}
 	
