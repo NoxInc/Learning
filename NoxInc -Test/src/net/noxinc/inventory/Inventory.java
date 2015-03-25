25718d04dac4c8d9056decdc515f6ab0bacf14c0
NoxInc <yolo632@byom.de>
2015-03-25 18:10:34 +0100

    ...

--- NoxInc -Test/src/net/noxinc/inventory/Inventory.java
+++ NoxInc -Test/src/net/noxinc/inventory/Inventory.java
@@ -1,19 +1,23 @@
 package net.noxinc.inventory;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import net.noxinc.status.Dialogs;
+import net.noxinc.units.Player;
 import net.noxinc.world.cells.Cell;
 
 public class Inventory 
 {
 	private ArrayList<Cell> inventory;
 	private int maxSize;
+	private Player player;
 	
-	public Inventory(int maxSize)
+	public Inventory(int maxSize, Player player)
 	{
 		inventory = new ArrayList<Cell>();
 		this.maxSize = maxSize;
+		this.player = player;
 	}
 	
 	public void addToInventory(Cell cell)
@@ -30,7 +34,8 @@ public class Inventory
 	{
 		String tmp = " ";
 		String tmp2 = "";
-		int tmp3 = maxSize - inventory.size();
+		int tmp3;
+		Scanner myScanner = new Scanner(System.in);
 		for(int i = 0; i < inventory.size(); i++)
 		{
 			tmp += "_ ";
@@ -44,5 +49,17 @@ public class Inventory
 			tmp2 += "| ";
 		}
 		System.out.println(tmp + "\n" + tmp2 + "|");
+		
+		Dialogs.inventory1();
+		
+		tmp3 = myScanner.nextInt();
+		if(!(tmp3 > maxSize + 1))
+		{
+			inventory.get(tmp3 - 1).use(player);
+		}else{
+			player.getMap().drawMap(player);
+		}
+		
+		
 	}
 }
