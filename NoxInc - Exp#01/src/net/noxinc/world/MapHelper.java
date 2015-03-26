1a3d7792f7dd6e465b03d87c00a13541c38c55a0
NoxInc <yolo632@byom.de>
2015-03-26 05:33:36 +0100

    Increased chance for unique name

--- NoxInc - Exp#01/src/net/noxinc/world/MapHelper.java
+++ NoxInc - Exp#01/src/net/noxinc/world/MapHelper.java
@@ -5,7 +5,7 @@ public class MapHelper
 {
 	public static String randomMapName()
 	{
-		double tmp = Math.round((Math.random() * 2));
+		double tmp = Math.round((Math.random() * 10000));
 		Double tmpDouble = new Double(tmp);
 		String s = tmpDouble.toString();
 		return s;
