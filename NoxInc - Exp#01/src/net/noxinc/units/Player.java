aee6225958bf226a47159718d3f385e533cb94ef
NoxInc <yolo632@byom.de>
2015-03-26 06:49:18 +0100

    Added player name; deletes collected items (map)

--- NoxInc - Exp#01/src/net/noxinc/units/Player.java
+++ NoxInc - Exp#01/src/net/noxinc/units/Player.java
@@ -1,18 +1,21 @@
 package net.noxinc.units;
 
 import net.noxinc.world.Map;
+import net.noxinc.world.cells.Cell;
 
 public class Player 
 {
 	private Map map;
+	private String name;
 	private int facingDirection;
 	private int x;
 	private int y;
 	private boolean isdead;
 	
-	public Player(Map map, int x, int y)
+	public Player(String name, Map map, int x, int y)
 	{
 		this.map = map;
+		this.name = name;
 		this.x = x;
 		this.y = y;
 		map.registerPlayer(this);
@@ -23,6 +26,10 @@ public class Player
 		this.facingDirection = facingDirection;
 		if(!(x + oX < 1 || x + oX > map.getMapX() - 2) && !(y + oY < 1 || y + oY > map.getMapY() - 2))
 		{
+			if(map.getCell(x + oX, y + oY).isCollectable())
+			{
+				map.setCell(new Cell(' ', "Air"), x + oX, y + oY);
+			}
 			x += oX;
 			y += oY;
 		}
@@ -56,5 +63,13 @@ public class Player
 	public void setDead()
 	{
 		isdead = true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
