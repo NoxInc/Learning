a4775c35745b33ca31016ea078d2cc8b45043e41
NoxInc <yolo632@byom.de>
2015-03-26 05:34:16 +0100

    Added tudo

--- NoxInc - Exp#01/src/net/noxinc/world/MapHelper.java
+++ NoxInc - Exp#01/src/net/noxinc/world/MapHelper.java
@@ -5,6 +5,7 @@ public class MapHelper
 {
 	public static String randomMapName()
 	{
+		//TODO Only unique names
 		double tmp = Math.round((Math.random() * 10000));
 		Double tmpDouble = new Double(tmp);
 		String s = tmpDouble.toString();
