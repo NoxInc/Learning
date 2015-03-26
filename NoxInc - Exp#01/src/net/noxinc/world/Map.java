42c62c682f72cc3da5d10c93c59b43dba6a24b5b
NoxInc <yolo632@byom.de>
2015-03-26 05:33:20 +0100

    Added updateMap(), craeteBoard()

--- NoxInc - Exp#01/src/net/noxinc/world/Map.java
+++ NoxInc - Exp#01/src/net/noxinc/world/Map.java
@@ -1,9 +1,13 @@
 package net.noxinc.world;
 
+import java.util.ArrayList;
+
+import net.noxinc.units.Player;
 import net.noxinc.world.cells.Cell;
 
 public class Map 
 {
+	private Player player;
 	private Cell[][] board;
 	private String name;
 	
@@ -21,6 +25,51 @@ public class Map
 	{
 		board = new Cell[y][x];
 		this.name = name;
+		createBoard();
+	}
+	
+	public void updateMap()
+	{
+		String tmp = "";
+		int counter = 0;
+		for(int y = counter; y < board.length; y++)
+		{
+			for(int x = 0; x < board[y].length; x++)
+			{
+				if(board[y][x] == null)
+				{
+					tmp += '.';
+				}else{
+					if(player.getX() == x && player.getY() == y)
+					{
+						tmp += 'P';
+					}else{
+						tmp += board[y][x].getSymbol();
+					}
+				}
+			}
+			if(!(y < y - 1))
+			{
+				tmp += "\n";
+			}
+		}
+		System.out.print(tmp + "\n");
+	}
+	
+	public void registerPlayer(Player player)
+	{
+		this.player = player;
+	}
+	
+	private void createBoard()
+	{
+		for(int y = 1; y < board.length - 1; y++)
+		{
+			for(int x = 1; x < board[y].length - 1; x++)
+			{
+				board[y][x] = new Cell(' ', "Air");
+			}
+		}
 	}
 	
 	public String getMapName()
